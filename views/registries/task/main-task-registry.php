d9d75ecf64f35d2066d0237849337bb8012fd44b
camilo-diamante-jr <camilodiamantejr@gmail.com>
2025-06-11 15:52:22 +0800

    nothing to commit with

---
+++ views/registries/task/main-task-registry.php
@@ -0,0 +1,7 @@
+<section class="container">
+    <div class="card">
+        <div class="card-body">
+            table>thead
+        </div>
+    </div>
+</section>
--- views/task.php
+++ views/task.php
@@ -1,35 +1,4 @@
-<section class="container mt-5">
-    <div class="card">
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Employee</th>
-                            <th>Task</th>
-                            <th>Description</th>
-                            <th>Deadline</th>
-                            <th>Status</th>
-                            <th>Remarks</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Austine J. Smith</td>
-                            <td>Liquidations</td>
-                            <td>Office supplies liquidation</td>
-                            <td>June 11, 2025</td>
-                            <td>Pending</td>
-                            <td>On going</td>
-                            <th>
-                                <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#updateModal">Update</button>
-                                <button type="button" class="btn btn-sm btn-danger">Remove</button>
-                            </th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</section>
+<?php
+
+
+include 'registries/task/main-task-registry.php';
